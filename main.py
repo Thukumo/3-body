f66d9092b918557d60cfbd034cf66ed00e3db8d7
つくも <92912896+Thukumo@users.noreply.github.com>
2024-10-05 23:15:41 +0900

    mul!!

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import scipy, itertools
+import scipy, itertools, scipy.constants
 from collections import namedtuple
 import numpy as np
-import scipy.constants
 from math import sqrt
 from tqdm import tqdm
 
@@ -21,13 +20,14 @@ range_xy = 1.0e2
 planets.append(planet(0, 0, 5.972e3, vector(0, 0), "red"))
 planets.append(planet(0, 4.0e1, 7.0e2, vector(0.05, 0), "blue"))
 """
-delta_t = 5.0e3
+delta_t = 1.0e0
 range_xy = 1.0e2
 size_marker = 25
 draw_line = 0
 plot = 0
-frame = 1200
-fps = 40
+mul = int(5.0e3)
+frame = 1500
+fps = 30
 planets.append(planet(-6.0e1, -4.0e1, 5.972e3, vector(0, 0), "red"))
 planets.append(planet(6.0e1, 4.0e1, 7.0e2, vector(0, 0), "blue"))
 planets.append(planet(6.0e1, -4.0e1, 7.0e2, vector(0, 0), "green"))
@@ -74,20 +74,22 @@ for p in planets:
     x[-1], y[-1] = x[-1] - v[-1].x*delta_t, y[-1] - v[-1].y*delta_t
 del planets
 oldpoint = None
+if "mul" not in globals(): mul = 1
 
 def update(_):
     global num, oldpoint
-    num+=1
-    for n in range(n_planets): x[n], y[n] = x[n] + v[n].x*delta_t, y[n] + v[n].y*delta_t
-    for i, j in itertools.combinations(range(n_planets), 2):
-        xdiff = x[i] - x[j]
-        ydiff = y[i] - y[j]
-        if (x2py2 := xdiff**2 + ydiff**2) == 0: continue
-        x2py2sqrt = sqrt(x2py2)
-        cos, sin = xdiff/x2py2sqrt, ydiff/x2py2sqrt
-        fmm = scipy.constants.G*delta_t/x2py2
-        v[i], v[j] = vector(v[i].x - fmm*m[j]*cos, v[i].y - fmm*m[j]*sin), vector(v[j].x + fmm*m[i]*cos, v[j].y + fmm*m[i]*sin)
-    if (not draw_line) and oldpoint != None : oldpoint.remove()
+    for _ in range(mul):
+        num+=1
+        for n in range(n_planets): x[n], y[n] = x[n] + v[n].x*delta_t, y[n] + v[n].y*delta_t
+        for i, j in itertools.combinations(range(n_planets), 2):
+            xdiff = x[i] - x[j]
+            ydiff = y[i] - y[j]
+            if (x2py2 := xdiff**2 + ydiff**2) == 0: continue
+            x2py2sqrt = sqrt(x2py2)
+            cos, sin = xdiff/x2py2sqrt, ydiff/x2py2sqrt
+            fmm = scipy.constants.G*delta_t/x2py2
+            v[i], v[j] = vector(v[i].x - fmm*m[j]*cos, v[i].y - fmm*m[j]*sin), vector(v[j].x + fmm*m[i]*cos, v[j].y + fmm*m[i]*sin)
+    if num != mul-1 and not draw_line : oldpoint.remove()
     ax.set_aspect("equal")
     oldpoint = plt.scatter(x, y, c=colors, s=size_marker)
     plt.title(f"{n_planets}体問題のシミュ (t={delta_t*num}s, dt={delta_t}s)", fontname="MS Gothic")
