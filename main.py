0a1d910030613c8ea4deb62a6acbc6168138df19
つくも <92912896+Thukumo@users.noreply.github.com>
2024-10-05 21:39:58 +0900

    ちょっと効率よくした

--- main.py
+++ main.py
@@ -6,16 +6,12 @@ from collections import namedtuple
 import numpy as np
 import scipy.constants
 from math import sqrt
+from tqdm import tqdm
 
 #いろいろの初期化とか
 vector = namedtuple("vector", ["x", "y"])
 planet = namedtuple("planet", ["x", "y", "m", "v", "color"]) #(m), (m), vector(x, y), color
-planets = []
-x = []
-y = []
-m = []
-v = []
-colors = []
+planets, x, y, m, v, colors = [], [], [], [], [], []
 
 # シミュレーション設定
 #delta_t = 1.0e7
@@ -25,12 +21,18 @@ range_xy = 1.0e2
 planets.append(planet(0, 0, 5.972e3, vector(0, 0), "red"))
 planets.append(planet(0, 4.0e1, 7.0e2, vector(0.05, 0), "blue"))
 """
-"""
+delta_t = 5.0e3
+range_xy = 1.0e2
+size_marker = 25
+draw_line = 0
+plot = 0
+frame = 1200
+fps = 40
 planets.append(planet(-6.0e1, -4.0e1, 5.972e3, vector(0, 0), "red"))
 planets.append(planet(6.0e1, 4.0e1, 7.0e2, vector(0, 0), "blue"))
 planets.append(planet(6.0e1, -4.0e1, 7.0e2, vector(0, 0), "green"))
-"""
 
+"""
 delta_t = 5.0e2
 range_xy = 1.5
 size_marker = 15
@@ -38,6 +40,7 @@ draw_line = False
 planets.append(planet(0, 0, 100, vector(0, 0), "red"))
 planets.append(planet(0, 1, 1.0e-2, vector(-sqrt(scipy.constants.G*100), 0), "blue"))
 #planets.append(planet(1, 0, 1.0e-2, vector(0, -sqrt(scipy.constants.G*100)), "green"))
+"""
 """
 range_xy = 2e10
 delta_t = 8.64e4*4
@@ -50,8 +53,8 @@ planets.append(planet(0.1e11, 0.01e11, situryo, vector(0, -1.0e2), "blue"))
 delta_t = 2.0e3
 range_xy = 1.0e2
 size_marker = 5
-draw_line = False
-for _ in range(100): planets.append(planet(np.random.randint(-range_xy, range_xy), np.random.randint(-range_xy, range_xy), np.random.randint(1, 10) * 10 ** np.random.randint(2, 4), vector(0, 0), "red"))
+draw_line = 1
+for _ in range(3): planets.append(planet(np.random.randint(-range_xy, range_xy), np.random.randint(-range_xy, range_xy), np.random.randint(1, 10) * 10 ** np.random.randint(2, 4), vector(0, 0), "red"))
 """
 
 
@@ -70,10 +73,10 @@ for p in planets:
     colors.append(p.color)
     x[-1], y[-1] = x[-1] - v[-1].x*delta_t, y[-1] - v[-1].y*delta_t
 del planets
+oldpoint = None
 
 def update(_):
-    global num
-    #print(v)
+    global num, oldpoint
     num+=1
     for n in range(n_planets): x[n], y[n] = x[n] + v[n].x*delta_t, y[n] + v[n].y*delta_t
     for i, j in itertools.combinations(range(n_planets), 2):
@@ -84,19 +87,21 @@ def update(_):
         cos, sin = xdiff/x2py2sqrt, ydiff/x2py2sqrt
         fmm = scipy.constants.G*delta_t/x2py2
         v[i], v[j] = vector(v[i].x - fmm*m[j]*cos, v[i].y - fmm*m[j]*sin), vector(v[j].x + fmm*m[i]*cos, v[j].y + fmm*m[i]*sin)
-    if not draw_line: plt.cla()
+    if (not draw_line) and oldpoint != None : oldpoint.remove()
     ax.set_aspect("equal")
-    plt.scatter(x, y, c=colors, s=size_marker)
+    oldpoint = plt.scatter(x, y, c=colors, s=size_marker)
     plt.title(f"{n_planets}体問題のシミュ (t={delta_t*num}s, dt={delta_t}s)", fontname="MS Gothic")
-    plt.xlabel('X (m)')
-    plt.ylabel('Y (m)')
-    plt.grid(True)
-    plt.xlim(-range_xy, range_xy)
-    plt.ylim(-range_xy, range_xy)
 
 fig = plt.figure()
 ax = plt.axes()
-ani = animation.FuncAnimation(fig, update, interval=interval_update, frames=1000)
-#ani.save("test.gif", writer = 'imagemagick')
-# グラフを表示
-plt.show()
+plt.xlabel('X (m)')
+plt.ylabel('Y (m)')
+plt.grid(True)
+plt.xlim(-range_xy, range_xy)
+plt.ylim(-range_xy, range_xy)
+if "frame" in globals(): ani = animation.FuncAnimation(fig, update, interval=interval_update, frames=tqdm(range(frame)))
+else: ani = animation.FuncAnimation(fig, update, interval=interval_update)
+if plot:
+    plt.show()
+else:
+    ani.save("test.gif", writer = 'imagemagick', fps=fps)
